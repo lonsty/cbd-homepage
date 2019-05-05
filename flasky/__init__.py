18b90eac640aad374b63a6f0cb9d68f9e82f37e3
lonsty <lonsty@sina.com>
2019-05-05 16:39:24 +0800

    fixed won't run on production environment

--- flasky/__init__.py
+++ flasky/__init__.py
@@ -7,7 +7,7 @@ from config import config
 
 def create_app(config_name=None):
     if config_name is None:
-        config_name = os.environ.get('FLACK_CONFIG', 'development')
+        config_name = os.environ.get('FLASK_CONFIG', 'development')
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
